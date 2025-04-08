036d419095bca94d11fe2120d296adfe1c4f7484
Leonisaurov <leonisaurov@gmail.com>
2025-04-08 17:17:00 -0600

    Improving the game cursor visibility, disabling the native cursor

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <termios.h>
 #include "menu.h"
+#include <stdio.h>
 
 int main() {
     struct termios raw_term;
@@ -13,8 +14,13 @@ int main() {
     raw_term.c_lflag &= ~ICANON;
     tcsetattr(STDIN_FILENO, TCSANOW, &raw_term);
 
+    printf("\x1b[?25l");
+    fflush(stdout);
+
     int res = main_menu();
 
+    printf("\x1b[?25h");
+    fflush(stdout);
     tcsetattr(STDIN_FILENO, TCSANOW, &original_term);
     return res;
 }
